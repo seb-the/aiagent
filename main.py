09a82dbdfda9c03c9361864e86e18f4f0ec6aed1
seb-the <sebastian.theunissen@gmail.com>
2025-11-02 18:28:23 +0100

    add system_prompt

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
 user_prompt = sys.argv[1]
 
 messages = [
@@ -19,7 +21,8 @@ def generate_response(prompt):
 
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
-        contents = prompt
+        contents = prompt,
+        config = types.GenerateContentConfig(system_instruction = system_prompt),
     )
     print(response.text)
     return response
